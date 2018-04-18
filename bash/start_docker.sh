a7b2687c057350bab10922feab6b126fb29a7a9c
LeeWilli <wang.li.neu@gmail.com>
2018-04-18 16:36:28 +0800

    add bash file

--- bash/start_docker.sh
+++ bash/start_docker.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+DETECTRON=/detectron
+sudo docker run --rm -it \
+--name detectron \
+--mount type=bind,source=/data/wangli/code/Detectron,target=$DETECTRON \
+--mount type=bind,source=/data/wangli/fashionai_keypoint,target=$DETECTRON/lib/datasets/data/fashionai_keypoint \
+--mount type=bind,source=/data/wangli/tmp,target=/tmp \
+detectron:c2-cuda9-cudnn7 bash
